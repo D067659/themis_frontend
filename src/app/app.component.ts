5bcf900f32c2681e05dc1084726c9e66efd2ce89
D067659 <peter.widmer@sap.com>
2022-02-15 10:29:55 +0100

    adjusted logout part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,13 @@ export class AppComponent {
     { title: 'Home', url: '/home', icon: 'home' },
     { title: 'Verein', url: '/club', icon: 'people-circle' },
     { title: 'Spiele', url: '/matches', icon: 'megaphone' }
-    // { title: 'Profil', url: '/profile', icon: 'person' }
   ];
 
-  constructor(public authService: ApiService, private router: Router) { }
+  public accountPages = [
+    { title: 'Profil', url: '/profile', icon: 'person' }
+  ]
+
+  constructor(public authService: ApiService) { }
 
   async logout() {
     await this.authService.logout();
