5e5e9051fc579f24869a84aea8efff97b8b81d0d
D067659 <peter.widmer@sap.com>
2022-08-15 12:07:20 +0200

    Revert "new angular"
    This reverts commit 28f035a397bcb94029a084cc00132ab461c349c7.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,13 +18,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 registerLocaleData(localeDe, 'de');
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule],
-    providers: [
-        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-        { provide: LOCALE_ID, useValue: 'de' }
-    ],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: LOCALE_ID, useValue: 'de' }
+
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
